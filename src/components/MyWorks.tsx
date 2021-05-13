0ec88ec1379a77cd6a0b03a36e62fc0ef9173371
kei <keisuketanaka97@gmail.com>
2021-05-13 21:31:49 +0200

    added my works, small stylesheet changes

--- src/components/MyWorks.tsx
+++ src/components/MyWorks.tsx
@@ -5,6 +5,10 @@ import Description from "./MyWorks.Description";
 import buddeImg from "../images/budde-kaffee.jpeg";
 import coffeeRecipeImg from "../images/coffee-recipe-app.jpeg";
 import myWebSiteImg from "../images/my-web-site.jpeg";
+import typingGameImg from "../images/typing_game.jpg";
+import slotMachineImg from "../images/slot_machine.jpg";
+import newsAppImg from "../images/news-app.jpg";
+import stopWatchImg from "../images/stop-watch.jpg";
 
 const MyWorks: React.FC = () => {
   return (
@@ -12,12 +16,21 @@ const MyWorks: React.FC = () => {
       <h2>MY WORKS</h2>
       <div className="my-works-container">
         <Description
+          className="nachrichten-heute"
+          src={newsAppImg}
+          alt="News App"
+          link=""
+          title="Nachrichten Heute"
+          description="SPA that displays news from many different sources."
+          usedTech={["TypeScript", "React", "HTML5", "SCSS"]}
+        />
+        <Description
           className="coffee-recipe-forum"
           src={coffeeRecipeImg}
           alt="Coffee Recipe Forum"
           link="https://coffee-app-252a4.web.app/"
           title="Coffee Recipe Forum"
-          description="this is a great tool"
+          description="A forum which can be post and browse coffee recipes."
           usedTech={["TypeScript", "React", "Firebase", "HTML5", "CSS3"]}
         />
         <Description
@@ -26,7 +39,7 @@ const MyWorks: React.FC = () => {
           alt="my website"
           link="#main-top"
           title="My Website"
-          description="The page witch you are currently visiting"
+          description="The page witch you are currently visiting."
           usedTech={["TypeScript", "React", "HTML5", "SCSS"]}
         />
         <Description
@@ -35,7 +48,34 @@ const MyWorks: React.FC = () => {
           alt="Budde Kaffee Website"
           link="https://buddekaffee.de/"
           title="Budde Kaffee Website"
-          description="this is a great website!"
+          description="Website for local coffee business."
+          usedTech={["HTML5", "CSS3", "JavaScript"]}
+        />
+        <Description
+          className="typing-name"
+          src={typingGameImg}
+          alt="Typing Game"
+          link=""
+          title="Typing Game"
+          description="Simple typing game developed with vanilla JS."
+          usedTech={["HTML5", "CSS3", "JavaScript"]}
+        />
+        <Description
+          className="slot-machine"
+          src={slotMachineImg}
+          alt="Slot Machine"
+          link=""
+          title="Slot Machine"
+          description="Slot Machine. Enjoy!"
+          usedTech={["HTML5", "CSS3", "JavaScript"]}
+        />
+        <Description
+          className="stop-watch"
+          src={stopWatchImg}
+          alt="Stop Watch"
+          link=""
+          title="Stop Watch"
+          description='Stop Watch with "Start", "Stop" and "Reset" button. My very first project.'
           usedTech={["HTML5", "CSS3", "JavaScript"]}
         />
       </div>
